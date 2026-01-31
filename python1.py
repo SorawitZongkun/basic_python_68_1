9409c24a036ef6be19705c31138d0d37fa03bd78
Sorawit <thankzaki@hotmail.com>
2026-01-31 13:18:22 +0700

    Python Variables

---
+++ python1.py
@@ -0,0 +1,51 @@
+"""
+#
+# Part: Python Comment
+#
+"""
+# This is a single line comment in Python
+
+"""
+v = s / t
+ความเร็ว = ระยะทาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World!")  # This will print a greeting message
+
+"""
+#
+# Part: Python Variables
+#
+"""
+x = 10  # Assigning integer value to variable x
+y = "10" # Assigning string value to variable y
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Variable Naming
+#
+"""
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+# my-var = 600  # This will cause a syntax error due to the hyphen
+# 2myvar = 700  # This will cause a syntax error because variable names cannot start with a number
+# my var = 800  # This will cause a syntax error due to the space
+
+# Camel Case
+myVariableName = 900
+# Pascal Case
+MyVariableName = 1000
+# Snake Case
+my_variable_name = 1100
